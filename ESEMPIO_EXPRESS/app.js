067620ada6aece4fc0bbad5881802f912319c73c
Molodduwa <molodduwa.andri@iisgalvanimi.edu.it>
2023-11-23 08:38:47 +0000

    LAVORO FINITO

--- ESEMPIO_EXPRESS/app.js
+++ ESEMPIO_EXPRESS/app.js
@@ -1,20 +1,18 @@
+const path = require('path');
 var express = require('express');
 var app = express();
-const path= require('path')
 
-app.get('/index.html', function(req,res){
-    res.sendFile(path.join(__dirname,'index.html')); //dirname: ritorna la cartella del progetto
+app.get('/', function (req, res){
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.get('/about.html', function(req,res){
-    res.sendFile(path.join(__dirname,'about.html')); //dirname: ritorna la cartella del progetto
+app.get('/about', function(req, res){
+    res.sendFile(path.join(__dirname, 'about.html'))
 });
 
-app.get('/sitemap.html', function(req,res){
-    res.sendFile(path.join(__dirname,'sitemap.html')); //dirname: ritorna la cartella del progetto
+app.get('/sitemap', function(req, res){
+    res.sendFile(path.join(__dirname, 'sitemap.html'))
 });
-
-
-app.listen(3000, function(){
-    console.log('Example app listening on port 3000!')
+app.listen(3000, function () {
+    console.log('Example app listening on port 3000');
 });
